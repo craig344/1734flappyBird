571dc581d52721f25bec594b5bff7fd6de59ee04
Craig Da Silva <craig_344@hotmail.com>
2018-10-23 16:37:54 +0530

    Added renderer

--- Flappy Bird/src/flappyBird/FlappyBird.java
+++ Flappy Bird/src/flappyBird/FlappyBird.java
@@ -1,20 +1,44 @@
  package flappyBird;
 
-import javax.swing.JFrame;
+import java.awt.Graphics;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
-public class FlappyBird {
+import javax.swing.JFrame;
+import javax.swing.Timer;
+
+public class FlappyBird implements ActionListener {
 	public static FlappyBird flappyBird;
 	public final int WIDTH = 800, HEIGHT=800;
+	public Renderer renderer;
+	Timer timer = new Timer(20, this);
+	
+	
 	public FlappyBird() {
 		JFrame jframe = new JFrame();
 		
+		renderer = new Renderer();
+		
+		jframe.add(renderer);
 		jframe.setResizable(false);
 		jframe.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		jframe.setSize(WIDTH,HEIGHT);
 		jframe.setVisible(true);
 		
 	}
+	
+	@Override
+	public void actionPerformed(ActionEvent e) {
+		// TODO Auto-generated method stub
+		renderer.repaint();
+	}
+	
 	public static void main(String[] args) {
 		flappyBird = new FlappyBird();
 	}
+	public void repaint(Graphics g) {
+		// TODO Auto-generated method stub
+		System.out.println("Hello");
+	}
+	
 }
